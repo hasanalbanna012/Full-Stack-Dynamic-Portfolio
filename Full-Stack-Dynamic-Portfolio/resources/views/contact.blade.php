1e4add07ee88c31c9362cd3b6d906c17adf2a354
hasanalbanna012 <shahhasanta@gmail.com>
2025-06-26 15:51:41 +0600

    added

---
+++ Full-Stack-Dynamic-Portfolio/resources/views/contact.blade.php
@@ -0,0 +1,24 @@
+
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+<section class="contact">
+    <h2>Get in Touch</h2>
+    <p>Based in Bangladesh</p>
+    <div class="social-icons">
+  <p><a href="https://facebook.com/yourprofile" target="_blank"><i class="fab fa-facebook-f"></i></a>facebook</p>
+  <a href="https://instagram.com/yourprofile" target="_blank"><i class="fab fa-instagram"></i></a>
+  <a href="https://linkedin.com/in/yourprofile" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+</div>
+
+    <div class="services">
+      <h3>Services</h3>
+      <ul>
+        <li>App Development</li>
+        <li>Digital Marketing</li>
+      </ul>
+    </div>
+  </section>
+@endsection
